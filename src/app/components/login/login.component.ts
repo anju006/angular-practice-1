936487ff334edb92ed96ffd555571251926f3db5
anju006 <anju.hembram06@gmail.com>
2022-02-27 20:35:35 +0530

    new changes

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  faLock = faLock;
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  constructor(private auth: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(): void{
+    if(this.loginForm.valid){
+      this.auth.login(this.loginForm.value).subscribe(
+        (result) => {
+          this.router.navigate(['admin']);
+        },
+        (err: Error) => {
+          alert(err.message);
+        }
+      );
+    }
+  }
+
+}
